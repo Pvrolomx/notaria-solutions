626dcc16e06440f587ebe9da8791c5a7e66a1a84
Pvrolomx <pvrolo@gmail.com>
2026-01-05 06:46:59 -0600

    fix: bump cache v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para Notaria Solutions PWA
-const CACHE_NAME = 'notaria-solutions-v2';
+const CACHE_NAME = 'notaria-solutions-v3';
 const urlsToCache = [
   '/',
   '/calculadora.html',
